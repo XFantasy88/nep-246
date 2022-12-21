d46761c11ab678c10be217b95b530bcd0e40b2cc
XFantasy88 <erencote@gmail.com>
2022-12-21 04:14:40 -0500

    approval mod

--- src/multi_token/approval/approval_receiver.rs
+++ src/multi_token/approval/approval_receiver.rs
@@ -1,4 +1,4 @@
-use crate::non_fungible_token::token::TokenId;
+use crate::multi_token::token::TokenId;
 use near_sdk::{ext_contract, AccountId};
 
 /// Approval receiver is the trait for the method called (or attempted to be called) when an MT contract adds an approval for an account.
